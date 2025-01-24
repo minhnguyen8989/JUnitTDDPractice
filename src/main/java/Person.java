1b2e57f9896654c2f3ce1dc4f2f7c20f70c10664
minh Nguyen <quangminh.nguyen8989@gmail.com>
2025-01-23 21:10:11 -0600

    Continue with Calculator class test

---
+++ src/main/java/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+
+    String firstName = "Minh";
+    String lastName = "Khan";
+    int age = 23;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -2,16 +2,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class JUnitTest {
+
+    //Calculator Class Test
     @Test
     public void testAddition() {
         Calculator calculator = new Calculator();
         int result = calculator.add(2, 3);
         assertEquals(5, result, "Addition should return the sum of the two numbers");
     }
-
     @Test
     public void testSubtraction() {
         Calculator calculator = new Calculator();
         int result = calculator.subtract(5, 3);
-        assertEquals(2, result, "Subtraction should return the difference of the two numbers");}
+        assertEquals(2, result, "Subtraction should return the difference of the two numbers");
+    }
+
+    @Test
+    public void testMultiply() {
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(5, 3);
+        assertEquals(15, result, "Multiplication should return the difference of the two numbers");
+    }
+    @Test
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        double result = calculator.divide(6,3);
+        assertEquals(2, result, "Divide should return the difference of the two numbers");
+    }
+
+
+    //Person Class Test
 }
