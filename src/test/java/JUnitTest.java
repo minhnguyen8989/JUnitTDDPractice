f619380a99488a5fe4ea57b7ae469f83757c4aa1
minh Nguyen <quangminh.nguyen8989@gmail.com>
2025-01-23 20:24:40 -0600

    first test with calculator

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -1,18 +1,17 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-public class CalculatorTest {
-
+public class JUnitTest {
     @Test
     public void testAddition() {
         Calculator calculator = new Calculator();
         int result = calculator.add(2, 3);
         assertEquals(5, result, "Addition should return the sum of the two numbers");
     }
+
     @Test
     public void testSubtraction() {
         Calculator calculator = new Calculator();
         int result = calculator.subtract(5, 3);
         assertEquals(2, result, "Subtraction should return the difference of the two numbers");}
-
 }
